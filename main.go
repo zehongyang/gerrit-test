63769fdcef47640f2c34d158a9ed3cc82eb05c75
zehongyang <532945788@qq.com>
2024-01-07 12:00:59 +0800

    hh

--- main.go
+++ main.go
@@ -2,8 +2,21 @@ package main
 
 import "fmt"
 
+type MyInt interface {
+	int | int8 | int16 | int32 | int64
+}
+
+func GetMaxNum[T MyInt](a, b T) T {
+	if a > b {
+		return a
+	}
+
+	return b
+}
+
 func main() {
 	fmt.Println(1)
 	fmt.Println("test")
 	fmt.Println("1111")
+	GetMaxNum(1, 2)
 }
